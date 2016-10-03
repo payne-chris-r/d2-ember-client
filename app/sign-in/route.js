bfb2b1e70e6a45d7b18a2fc635343cea57e53f52
payne-chris-r <06cpayne@gmail.com>
2016-10-02 20:36:40 -0700

    Trying to solve sign-up->sign-in->create profile
    Currently when a user signs up they also sign in. I
    want them to also be transitioned to the 'profile-edit'
    page so that they can create a profile as well.
    NOTE: I think I may have broken sign-out

--- app/sign-in/route.js
+++ app/sign-in/route.js
@@ -7,7 +7,28 @@ export default Ember.Route.extend({
   actions: {
     signIn (credentials) {
       return this.get('auth').signIn(credentials)
-      .then(() => this.transitionTo('profile', this.get('auth.credentials.profile_id')))
+      .then(() => {
+        console.log("inside promise ");
+        let profile_id = this.get('auth.credentials.profile_id');
+        if(profile_id){
+          this.transitionTo('profile', profile_id);
+          console.log("inside promise profile id is ", profile_id);
+        }
+        else{
+          let newProfile = {
+            user: this.get('auth.credentials.id')
+          };
+          // IS CREATE RECORD ASYNC?
+          newProfile = this.get('store').createRecord('profile', newProfile);
+          console.log("inside new profile is ", newProfile);
+          newProfile.save()
+            .then((profile)=>{
+              console.log("inside promise profile was not defined? It is ", profile.id);
+              this.transitionTo('profile-edit', profile.id);
+            })
+            .catch(console.error);
+        }
+      })
       // .then((user) => this.transitionTo('profile', user.profile_id))
       .then(() => this.get('flashMessages').success('Thanks for signing in!'))
       .catch(() => {
