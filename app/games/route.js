3580b22456d3278a07b69ef2d822680264f98dc1
payne-chris-r <06cpayne@gmail.com>
2016-10-17 16:33:40 -0400

    Add error handling to route
    Add error handling if the user isn't authenticated, or is already
    in the game, or ISN'T in the game and tries to leave

--- app/games/route.js
+++ app/games/route.js
@@ -24,24 +24,19 @@ export default Ember.Route.extend({
         .then((profile)=>{
           this.get('store').findRecord('game', +game.id)
             .then((game)=>{
-              console.log("game.players is ", game.players);
               newPlayer.set('game', game);
             })
             .then(()=>{
               newPlayer.set('character_id', 3);
               newPlayer.set('kills', 3);
             })
-            .then(()=>{
-              newPlayer.save()
-              .catch((err)=>{
-                console.error(err);
-                /// WHAT? HOW? <-- database is checking uniqueness
-                console.log("You can only join a game once!");
-                this.transitionTo('game', game.id);
-              });
-            })
+            .then(()=> newPlayer.save())
+            .then(()=> this.transitionTo('game', game.id))
             .catch((err)=>{
-              console.error(err);
+              // if err == user is already in that game
+              // elsif err = game is full.
+              this.get('flashMessages').warning('You are already in that game, or the game is currently full.');
+              this.transitionTo('game', game.id);
             });
         })
         .catch((err)=>{
@@ -49,29 +44,43 @@ export default Ember.Route.extend({
         });
     },
     leaveGame: function(game){
-      console.log("Game is ", game.id);
+      if(!(this.get('profile_id'))){
+        this.transitionTo('sign-in');
+        throw new Error("User needs to sign in.");
+      }
+
       this.get('store')
         .queryRecord('player', { game_id: game.id,
                                  profile_id: this.get('profile_id') })
-      .then((player)=>{
-        let playerProfileId = parseInt(player.get('profile.id'));
-        if(playerProfileId === this.get('profile_id')){
-          player.destroyRecord()
-            .then((what)=>{
-              console.log("What is ", what);
-            })
-            .catch((err)=>{
-              console.error(err);
-            });
-        }
-        else{
-          console.log("You don't have rights to remove that player");
-        }
+      // this doesn't work if user isn't IN that game.
+      // queryRecord returns `null`, but doesn't trigger `catch`
+
+
+      .then((player)=> player.destroyRecord())
+      // .then((player)=>{
+      //   console.log("Player is null? ", player);
+      //   let playerProfileId = parseInt(player.get('profile.id'));
+      //
+      //   if(playerProfileId === this.get('profile_id')){
+      //     player.destroyRecord()
+      //       .then((what)=>{
+      //         console.log("What is ", what);
+      //       })
+      //       .catch((err)=>{
+      //         console.error(err);
+      //       });
+      //   }
+      //   else{
+      //     console.log("You don't have rights to remove that player");
+      //   }
         // console.log("players 1st promise is", players);
         // console.log("players[0] 1st promise is", players.get('length'));
+      // })
+      .catch(()=>{
+        this.get('flashMessages').warning('You are not in that game.');
       })
-      .catch((err)=>{
-        console.error(err);
+      .then(() => {
+        this.transitionTo('game', game.id);
       });
     },
   },
