c08eca19d3d7535975a2b547b51aed2bd340758d
payne-chris-r <06cpayne@gmail.com>
2016-10-19 10:30:14 -0400

    Add styling to this beast
    Incorporate bootstrap template to try and make it look a little better.
    Add images as well.

--- app/components/my-application/component.js
+++ app/components/my-application/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   auth: Ember.inject.service(),
   store: Ember.inject.service(),
+  tagName: '',
 
   user: Ember.computed.alias('auth.credentials'),
   profile_id: Ember.computed.alias('auth.credentials.profile_id'),
