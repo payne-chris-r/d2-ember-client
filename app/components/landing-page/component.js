28104abf57058767f453ba95614bb91af561a63b
payne-chris-r <06cpayne@gmail.com>
2016-10-23 16:28:24 -0400

    Add landing-page component
    Add a langing page component to render the main page in the
    application/index route

---
+++ app/components/landing-page/component.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // classNameBindings: ['landingHidden'],
+  // landingHidden: false,
+  // actions: {
+  //   moveToGames () {
+  //     return this.toggleProperty('landingHidden');
+  //   },
+  // },
+});
