dfdd172a8d9a9bbde674531ffe85e246220b00fe
payne-chris-r <06cpayne@gmail.com>
2016-11-29 08:07:41 -0500

    Modify sign-in route for users without profile
    This should never happen, but we're error checking in the event
    that a user gets signed up with a user object, but no associated
    profile. Now on sign-in if they don't have a profile yet, a new
    one will be created for them.

--- app/sign-in/route.js
+++ app/sign-in/route.js
@@ -6,26 +6,34 @@ export default Ember.Route.extend({
 
   actions: {
     signIn (credentials) {
-      return this.get('auth').signIn(credentials)
-      .then((user) => {
-        let profile_id = this.get('auth.credentials.profile_id');
-        if(profile_id){
-          this.transitionTo('profile', profile_id);
-        }
-        else{
-          let newProfile = this.get('store').createRecord('profile')
-                                            .set('user', user);
-          newProfile.save()
-            .then((profile)=>{
-              this.transitionTo('profile/edit', profile.id);
-            })
-            .catch(console.error);
-        }
-      })
+      this.get('auth').signIn(credentials)
+        .then((user) => {
+          let profile_id = this.get('auth.credentials.profile_id');
+
+          if(profile_id){
+            this.transitionTo('profile', profile_id);
+          }
+          else{
+            let profile = this.get('store').createRecord('profile', { user });
+            profile.save()
+              .then((profile)=> {
+                this.get('auth.credentials').set('profile_id', profile);
+                // I don't think that profile_id should be set to profile
+                // shouldn't it be set to profile.get('id')??
+
+                // Next step:
+                // this.transitionTo('profile/edit', profile.get('id'));
+              })
+              .catch(() => {
+                this.get('flashMessages')
+                  .danger('There was a problem. Please try again.');
+              });
+          }
+        })
       .then(() => this.get('flashMessages').success('Thanks for signing in!'))
       .catch(() => {
         this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
+          .danger('There was a problem. Please try again.');
       });
     },
   },
