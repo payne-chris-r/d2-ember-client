18be8a2c89c50bd9aeb9c80d52d7c3fcbe5a38cc
payne-chris-r <06cpayne@gmail.com>
2016-12-16 20:45:40 -0500

    Adds new information to profiles
    Language, abandons, and reports are all now included from the api.
    Users can currently change that themselves, but in the future this
    will be handled by the steam api.

--- app/profile/model.js
+++ app/profile/model.js
@@ -5,12 +5,16 @@ export default DS.Model.extend({
   first_name: DS.attr('string'),
   last_name: DS.attr('string'),
   nationality: DS.attr('string'),
+  language: DS.attr('string'),
   ranking: DS.attr('number'),
+  abandons: DS.attr('number'),
+  reports: DS.attr('number'),
+
   carry: DS.attr('boolean'),
   support: DS.attr('boolean'),
+  mid: DS.attr('boolean'),
   offlaner: DS.attr('boolean'),
   jungler: DS.attr('boolean'),
-  mid: DS.attr('boolean'),
   roamer: DS.attr('boolean'),
 
   user: DS.belongsTo('user'),
